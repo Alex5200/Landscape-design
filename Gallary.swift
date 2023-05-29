674c703514ef2ad79346c3a19bf96e4b12b79311
Alex5200 <saha.aa35@gmail.com>
2023-05-29 20:58:46 +0300

    add file project

---
+++ Gallary.swift
@@ -0,0 +1,21 @@
+//
+//  gallary.swift
+//  ландшафтный дизайн
+//
+//  Created by Александр Ляхов on 27.05.2023.
+//
+
+import Foundation
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
---
+++ ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  ландшафтный дизайн
+//
+//  Created by Александр Ляхов on 24.05.2023.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
